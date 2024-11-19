512599caaf4e37bbae1c863c8587acc7c2c2999a
Kaynã Felipe <110180067+KaynaFelipe@users.noreply.github.com>
2024-11-19 00:23:06 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Kaynã Nutrição Perfeita (Melhor que o da Jéssica) 🌱"
+
+var pacientes = document.querySelector("h2");
+pacientes.textContent = "Pacientes cadastrados"
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
